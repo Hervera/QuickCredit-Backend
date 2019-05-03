27d9db7f4029fc400917bfec44da12973c93f2eb
Hervera <nkuliherve@gmail.com>
2019-05-03 18:04:41 +0200

    [Finishes #165749374, #165749362]

---
+++ UI/js/script.js
@@ -0,0 +1,37 @@
+function toggleRightMenu() {
+    document.getElementById("right-menu").classList.toggle("toggle-class");
+    // document.getElementById("down").style.transform = "rotate(-135deg)";
+}
+
+function toggleLeftMenu() {
+  document.getElementById("aside").classList.toggle("toggle-aside");
+  document.getElementById("content").classList.toggle("toggle-content");
+}
+
+
+var loginForm = document.getElementById('loginForm');
+loginForm.addEventListener('submit', function(e) {
+	
+	var admin = document.getElementById("email").value,
+	password = document.getElementById("password").value;
+
+	if(admin == "admin@quickcredit.com" && password == "secret"){
+		window.location.href = "admin/dashboard.html";
+	}
+	else{
+		window.location.href = "client/home.html";
+	}
+
+	e.preventDefault();
+}, true);
+
+
+function myFunction(e) {
+  var elems = document.querySelectorAll(".active");
+  [].forEach.call(elems, function(el) {
+    el.classList.remove("active");
+  });
+  e.target.className = "active";
+}
+
+	
