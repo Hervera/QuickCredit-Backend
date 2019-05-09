1eb93d12a4a22476c3ff85f36a909cc8351a0b27
Hervera <nkuliherve@gmail.com>
2019-05-09 09:47:29 +0200

    [Finish #165902940]

--- server/data/mock.js
+++ server/data/mock.js
@@ -31,11 +31,50 @@ const users = [
     password: bcrypt.hashSync('secret', 10),
     status: 'verified',
     address: 'Kigali, Kicukiro',
-    isAdmin: 0,
+    repaid: 0,
     createdOn: 'March 5th 2019, 07:00:00 am',
   },
 ];
 
+const loans = [
+  {
+    id: 1,
+    user: 'herveralive@gmail.com',
+    createdOn: 'March 3th 2019, 10:00:00 am',
+    status: 'pending',
+    tenor: 4,
+    repaid: 1,
+    amount: 550000,
+    paymentInstallment: 137500.025,
+    balance: 100000,
+    interest: 0.10,
+  },
+  {
+    id: 2,
+    user: 'brad@gmail.com',
+    createdOn: 'March 4th 2019, 03:00:00 pm',
+    status: 'approved',
+    tenor: 6,
+    repaid: 0,
+    amount: 100000,
+    paymentInstallment: 16666.7,
+    balance: 20000,
+    interest: 0.20,
+  },
+  {
+    id: 3,
+    user: 'johnLee@gmail.com',
+    createdOn: 'March 5th 2019, 07:00:00 am',
+    status: 'rejected',
+    tenor: 8,
+    repaid: 0,
+    amount: 200000,
+    paymentInstallment: 25000.03125,
+    balance: 50000,
+    interest: 0.25,
+  },
+];
+
 export default {
-  users,
+  users, loans,
 };
