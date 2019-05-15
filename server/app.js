7c5dbfdf532e3a594c3838eee00968b4ab0cf5b8
Hervera <nkuliherve@gmail.com>
2019-05-15 13:53:03 +0200

    [Chore #166029492] Update package.json for ci badges

--- server/app.js
+++ server/app.js
@@ -9,7 +9,7 @@ import swaggerDocument from '../swagger.json';
 
 const app = express();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 
 app.use(morgan('dev'));
 // Parse incoming requests data
