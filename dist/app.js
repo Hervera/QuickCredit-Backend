61eceb174e4517274623d40481c9bc6947a6856f
Hervera <nkuliherve@gmail.com>
2019-05-15 07:32:35 +0200

    [Bug #165996361] add Heroku scripts in package.json

--- dist/app.js
+++ dist/app.js
@@ -5,8 +5,6 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports["default"] = void 0;
 
-require("@babel/polyfill");
-
 var _express = _interopRequireDefault(require("express"));
 
 var _morgan = _interopRequireDefault(require("morgan"));
